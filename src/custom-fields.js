ed20c7225174bdbce8ac713bd4a96c0448cc620c
aznoqmous <paul.landreau931v@gmail.com>
2020-04-06 10:06:16 +0200

    ++

--- src/custom-fields.js
+++ src/custom-fields.js
@@ -3,13 +3,34 @@ import CustomInputNumber from './custom-input-number'
 
 export default class CustomFields {
     constructor(selector){
-        this.els = [...document.querySelectorAll(selector)]
-        this.els.map(el => this.build(el))
+        this.selector = selector
+        this.className = 'custom-element'
+        this.build()
+        this.bindObserver()
     }
 
-    build(el){
-        if(el.tagName == 'INPUT' && el.type == 'number') new CustomInputNumber(el)
-        if(el.tagName == 'SELECT') new CustomSelect(el)
+    bindObserver(){
+        this.observer = new MutationObserver(()=>{
+            this.build()
+        })
+        this.observer.observe(document.body, {
+            attributes: false,
+            childList: true,
+            subtree: true
+        })
     }
-    
+
+    build(){
+        this.els = [...document.querySelectorAll(this.selector)]
+        this.els.map(el => {
+            console.log(el)
+            // already bound
+            if(el.parentElement.classList.contains(this.className)) return false;
+
+            if(el.tagName == 'INPUT' && el.type == 'number') new CustomInputNumber(el)
+            if(el.tagName == 'SELECT') new CustomSelect(el)
+
+        })
+    }
+
 }
