e8e4c27b85c18ea6a05fa487086b1762379e4106
aznoqmous <paul.landreau931v@gmail.com>
2020-08-18 11:03:07 +0200

    fixed custom select interactions

--- src/custom-select.js
+++ src/custom-select.js
@@ -3,7 +3,7 @@ export default class CustomSelect extends CustomElement {
     constructor(el){
         super(el, 'custom-select')
 
-        this.shiftState = false
+        this.delay = 100
 
         this.build()
         this.bind()
@@ -46,79 +46,78 @@ export default class CustomSelect extends CustomElement {
 
     bind(){
 
+        /**
+        * Temporary events
+        */
+        this.keyup = (e)=>{
+            if(e.key == 'ArrowDown') this.onArrowDown()
+            if(e.key == 'ArrowUp') this.onArrowUp()
+            if(e.key == 'Enter') {
+                if(this.opened && Date.now() - this.opened  > this.delay) this.close()
+                else this.open()
+            }
+            e.preventDefault()
+        }
+
+        this.focusout = (e)=>{
+            if(!this.container.contains(e.target)) {
+                this.close()
+            }
+        }
+        this.keydown = (e)=>{
+            if(e.key == 'Tab') this.close()
+            else e.preventDefault()
+        }
+
         let options = [...this.optionsList.children]
         options.map(opt => {
             opt.addEventListener('click', ()=>{
                 this.setSelected(opt)
                 this.close()
-                window.removeEventListener('click', focusOut)
-                window.removeEventListener('keyup', keyup)
-                window.removeEventListener('keydown', keydown)
             })
         })
 
-        /**
-         * Temporary events
-         */
-        let keyup = (e)=>{
-
-            if(e.key == 'ArrowDown') this.onArrowDown()
-            if(e.key == 'ArrowUp') this.onArrowUp()
-            if(e.key == 'Enter') {
-                this.close()
-                window.removeEventListener('click', focusOut)
-                window.removeEventListener('keyup', keyup)
-                window.removeEventListener('keydown', keydown)
-            }
-            e.preventDefault()
-        }
-
-        let keydown = (e)=>{
-            e.preventDefault()
-        }
-
-        let focusOut = (e)=>{
-            if(!this.container.contains(e.target)) {
-                this.close()
-                window.removeEventListener('click', focusOut)
-                window.removeEventListener('keyup', keyup)
-                window.removeEventListener('keydown', keydown)
-            }
-        }
-
         this.el.addEventListener('focusin', (e)=>{
             this.open()
-            window.addEventListener('click', focusOut)
-            window.addEventListener('keyup', keyup)
-            window.addEventListener('keydown', keydown)
         })
 
         this.container.addEventListener('click', (e)=>{
             if(this.optionsList.contains(e.target)) return false;
             this.open()
-            window.addEventListener('click', focusOut)
-            window.addEventListener('keyup', keyup)
-            window.addEventListener('keydown', keydown)
         })
 
-        document.addEventListener('keyup', (e)=>{ if(e.key == 'Shift') this.shiftState = false })
-        document.addEventListener('keydown', (e)=>{ if(e.key == 'Shift') this.shiftState = true })
+        this.el.addEventListener('keydown', (e)=>{
+            if(!this.opened && e.key == 'Enter') {
+                setTimeout(()=>{this.open()}, 0)
+                e.preventDefault()
+            }
+        })
     }
 
     open(){
+        this.opened = Date.now()
         this.optionsList.classList.add('active')
+        window.addEventListener('click', this.focusOut)
+        window.addEventListener('keyup', this.keyup)
+        window.addEventListener('keydown', this.keydown)
     }
 
     close(){
+        this.opened = false
         this.optionsList.classList.remove('active')
+        window.removeEventListener('click', this.focusOut)
+        window.removeEventListener('keyup', this.keyup)
+        window.removeEventListener('keydown', this.keydown)
     }
 
     onArrowUp(){
         if(this.el.selectedIndex - 1 >= 0) this.setSelected(this.optionsList.children[this.el.selectedIndex - 1])
+        else this.close()
     }
 
     onArrowDown(){
-        if(this.el.selectedIndex + 1 < this.optionsList.children.length) this.setSelected(this.optionsList.children[this.el.selectedIndex + 1])
+        if(this.el.selectedIndex < 0) this.setSelected(this.optionsList.children[1])
+        else if(this.el.selectedIndex + 1 < this.optionsList.children.length) this.setSelected(this.optionsList.children[this.el.selectedIndex + 1])
     }
 
 }
